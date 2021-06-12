c2870b8baf3205490654896743f99236619646fa
Masud Rana <himu4960@gmail.com>
2021-06-13 03:24:03 +0600

    minifyed all files by parcel.js

---
+++ script.js
@@ -0,0 +1,23 @@
+const textEl = document.getElementById('text')
+const speedEl = document.getElementById('speed')
+const text ='Web Designer'
+
+let idx =1
+let speed = 300  / speedEl.value
+
+speedEl.addEventListener('input', (e)=> speed = 300 / e.target.value)
+
+writeText()
+
+function writeText(){
+   
+    textEl.innerHTML =` ${text.slice( 0, idx)}_`
+    idx++
+
+    if(idx > text.length){
+        idx=1
+    }
+
+    setTimeout(writeText,speed)
+
+}
